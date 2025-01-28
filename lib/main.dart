7db842d738cda4f4a99ee79d906c28371decefbe
melina <melina.bouchelleghem@edu.univ-fcomte.fr>
2025-01-28 15:03:06 +0100

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  //gfdgdfghdf
     // Test de 2 comdfgdf
   // Test d'un commentaire
   // This widget is the root of your application.
